8cf62c42b2ca6f9cecfabdda5ed7e18dcfe892f0
Emilien Lasguignes <emilien.lasguignes@inra.fr>
2019-10-09 17:51:56 +0200

    Emilien Lasguignes's avatar

--- scripts/intronSeeker.py
+++ scripts/intronSeeker.py
@@ -4,7 +4,7 @@
 import argparse 
 import sys 
 import os 
-from intronResearch import searchProtein,predictORF,truncate,split_research
+from intronSearch import searchProtein,predictORF,truncate,split_research
 from readsMapping import star,hisat2
 from dataSimulation import full_random_simulation,gtf_based_simulation,grinder
 from checkInstall import checkInstall
@@ -23,77 +23,50 @@ def parse_arguments() :
     
     # subparser to call STAR aligner
     parser_star = subparser.add_parser('starAlignement',add_help=False)
-    parser_star.add_argument('-r','--reference',
-                            type=str, dest='reference')
-    parser_star.add_argument('-1','--r1',
-                             help='fasta file of reads', type=str, dest='r1')
-    parser_star.add_argument('-2','--r2',
-                             help='second fasta file if reads if paired-end', type=str, default='',dest='r2')
-    parser_star.add_argument('-o','--prefix',
-                             help='output prefix', type=str, dest='prefix')
-    parser_star.add_argument('-t','--threads',
-                             help='number of threads used to perform alignement', type=int, default=1,dest='threads')
+    parser_star.add_argument('-r','--reference', type=str, dest='reference')
+    parser_star.add_argument('-1','--r1', type=str, dest='r1')
+    parser_star.add_argument('-2','--r2', type=str, default='',dest='r2')
+    parser_star.add_argument('-o','--output', type=str, dest='prefix')
+    parser_star.add_argument('-t','--threads', type=int, default=1,dest='threads')
     parser_star.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
     parser_star.set_defaults(func=star)
 
     # subparser to call Hisat2 aligner
-    parser_hisat2 = subparser.add_parser('hisat2Alignement',add_help=False, help='HiSat2 help. Needs aligner HiSat2-2.1.0 or more recent version')
-    parser_hisat2.add_argument('-r','--reference',
-                               help='reference file', type=str, dest='reference')
-    parser_hisat2.add_argument('-1','--r1',
-                               help='fasta file of reads', type=str, dest='r1')
-    parser_hisat2.add_argument('-2','--r2',
-                               help='second fasta file if reads if paired-end', type=str, default='', dest='r2')
-    parser_hisat2.add_argument('-o','--prefix',
-                               help='output prefix', type=str, default='HiSat2', dest='prefix')
-    parser_hisat2.add_argument('-t','--threads',
-                               help='number of threads used to perform alignement', type=int, default=1, dest='threads')
-    parser_hisat2.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
+    parser_hisat2 = subparser.add_parser('hisat2Alignement',add_help=False)
+    parser_hisat2.add_argument('-r','--reference', type=str, dest='reference')
+    parser_hisat2.add_argument('-1','--r1', type=str, dest='r1')
+    parser_hisat2.add_argument('-2','--r2', type=str, default='', dest='r2')
+    parser_hisat2.add_argument('-o','--output', type=str, default='HiSat2', dest='prefix')
+    parser_hisat2.add_argument('-t','--threads', type=int, default=1, dest='threads')
+    parser_hisat2.add_argument('-h','--help',action='store_const', const = parser_hisat2.prog.split()[-1],dest='c_help')
     parser_hisat2.set_defaults(func=hisat2)
     
     # subparser for the split read research
-    parser_split = subparser.add_parser('splitReadSearch',add_help=False, help='In the alignment file, look for split read')
-    parser_split.add_argument('-i', '--input',
-                              help='input bam file from alignment', dest='bamfilename', type=str, )
-    parser_split.add_argument('-r', '--reference',
-                              help='name of the reference file used for the alignment', type=str, dest='fastafilename')
-    parser_split.add_argument('-o','--prefix',
-                              help='prefix of ouput files',type=str, dest='basename')
-    parser_split.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
+    parser_split = subparser.add_parser('splitReadSearch',add_help=False)
+    parser_split.add_argument('-a', '--alignement', dest='bamfilename', type=str, )
+    parser_split.add_argument('-r', '--reference', type=str, dest='fastafilename')
+    parser_split.add_argument('-o','--output', type=str, dest='basename')
+    parser_split.add_argument('-h','--help',action='store_const', const = parser_split.prog.split()[-1],dest='c_help')
     parser_split.set_defaults(func=split_research)
 
     # subparser for writing fasta of spliced sequence
-    parser_trunc = subparser.add_parser('trimFastaFromGFF',add_help=False,help='Write fasta file where each sequence is spliced if it bears a feature (present in the gff file provided). If a sequence bears several features, a different spliced sequence is written for each feature')
-    parser_trunc.add_argument('-i', '--fasta',
-                              help='Fasta with sequences to splice', dest='fasta_file', type=str, )
-    parser_trunc.add_argument('-g', '--gff', 
-                              help='File with features to delete (GFF format only)', dest='gff_feature', type=str, )
-    parser_trunc.add_argument('-o','--output',
-                              help='Basename of the output file', dest='output', type=str, default='sequences')
-    parser_trunc.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
-    parser_trunc.set_defaults(func=truncate)
+    parser_trim = subparser.add_parser('trimFastaFromTXT',add_help=False)
+    parser_trim.add_argument('-r', '--reference', dest='fasta_file', type=str, )
+    parser_trim.add_argument('-f', '--features', dest='gff_feature', type=str, )
+    parser_trim.add_argument('-o','--output', dest='output', type=str, default='sequences')
+    parser_trim.add_argument('-h','--help',action='store_const', const = parser_trim.prog.split()[-1],dest='c_help')
+    parser_trim.set_defaults(func=truncate)
 
     # subparser for searching ORF on sequences
-    parser_orf = subparser.add_parser('analyzeORF',add_help=False, help='Run TransDecoder to search Open Reading Frame on sequences provided in fasta format.'
-                                                     'Returns the predicted ORFs in BED, nucleic FASTA, proteic FASTA and TSV format.'
-                                                     'Needs software TransDecoder-v5.5.0')
-    parser_orf.add_argument('-i', '--fasta', 
-                            help='Sequences on which the prediction will be performed', dest='fasta_file', type=str, )
-    parser_orf.add_argument('-t','--truncate-search',
-                            help='Boolean which says if, after the ORF prediction is performed on fasata files, a truncation (truncate module) is performed and another ORF prediction on truncated fasta',
-                            action='store_true',dest='trunc')
-    parser_orf.add_argument('-g', '--gff', 
-                            help='File with features to delete (GFF format only). Only used if -t option is present',dest='gff', type=str, required=False, default=None)
-    parser_orf.add_argument('-k','--keep_intermdiate',
-                            help='Boolean which rules intermediate files erasure (default False)', dest='rm', action='store_false',required=False, default=True)
-    parser_orf.add_argument('-o','--output',
-                            help='Basename of the output file', dest='output', type=str, default='predicted_orfs')
-    parser_orf.add_argument('--no_refine_starts', 
-                            help='Boolean which rules the refine start step of TransDecoder. Use it only if TransDecoder fails at this point.',dest='refine',action='store_false', required=False, default=True)
-    parser_orf.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
+    parser_orf = subparser.add_parser('analyzeORF',add_help=False)
+    parser_orf.add_argument('-r', '--reference', dest='fasta_file', type=str, )
+    parser_orf.add_argument('-k','--keep-intermediate', dest='rm', action='store_false',required=False, default=True)
+    parser_orf.add_argument('-o','--output', dest='output', type=str, default='predicted_orfs')
+    parser_orf.add_argument('--no-refine-starts', dest='refine',action='store_false', required=False, default=True)
+    parser_orf.add_argument('-h','--help',action='store_const', const = parser_orf.prog.split()[-1],dest='c_help')
     parser_orf.set_defaults(func=predictORF)
 
-    # subparser for aligning contigs and proteins to search long gaps (i.e. potential introns)
+    # subparser for aligning contigs and proteins
     parser_protein = subparser.add_parser('analyzeProtein',add_help=False, help='Run Diamond to search long gaps (i.e. potential introns) in contigs-protein alignement.'
                                                      'Returns a GFF file with all the found gaps'
                                                      'Needs Diamond-v0.9.9')
@@ -107,7 +80,7 @@ def parse_arguments() :
                                 help='Boolean which rules intermediate files erasure (default False)', dest='rm', action='store_true',required=False)
     parser_protein.add_argument('-t','--threads',
                                 help='Integer which indicates the numberof CPU to use for alignement (default 1)', dest='threads', type=int, required=False,default = 1)
-    parser_protein.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
+    parser_protein.add_argument('-h','--help',action='store_const', const = parser_protein.prog.split()[-1],dest='c_help')
     parser_protein.set_defaults(func=searchProtein)
 
     # subparser for Full Random Simulation
@@ -126,7 +99,7 @@ def parse_arguments() :
                             help='Maximal length of random intron. Default 1000', type=int, default=1000, dest='upper')
     parser_frs.add_argument('-o', '--output',
                             help='Basename of the generated files. Default [FullRandomSimulation] ', type=str, default='FullRandomSimulation', dest='output')
-    parser_frs.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
+    parser_frs.add_argument('-h','--help',action='store_const', const = parser_frs.prog.split()[-1],dest='c_help')
     parser_frs.set_defaults(func=full_random_simulation)
 
     # subparser for annotated genome-based data simulation (annoToData) 
@@ -144,7 +117,7 @@ def parse_arguments() :
                             help='prefix of ouput files', type=str, dest='output')
     parser_gbs.add_argument('--mix-library',
                             help='Boolean which rules if the generated library is mixed i.e. if the library contains the transcript in two state when a intron is retained or an exon is spliced', action='store_true', default=False, dest='mix')
-    parser_gbs.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
+    parser_gbs.add_argument('-h','--help',action='store_const', const = parser_gbs.prog.split()[-1],dest='c_help')
     parser_gbs.set_defaults(func=gtf_based_simulation)
 
     # subparser for grinder
@@ -155,12 +128,12 @@ def parse_arguments() :
                              help='Profile file : all arguments for grinder', type=str, required=False, default=os.path.abspath(os.path.dirname(sys.argv[0]) + '/../config/profile_file.txt'),dest='pf')
     parser_grinder.add_argument('-o','--pref',
                              help='Prefix of the output files', type=str, default='Grinder', dest='pref')
-    parser_grinder.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
+    parser_grinder.add_argument('-h','--help',action='store_const', const = parser_grinder.prog.split()[-1],dest='c_help')
     parser_grinder.set_defaults(func=grinder)
 
     # subparser for checkInstall
     parser_install = subparser.add_parser('checkInstall',add_help=False, help='Check module to verify if the running environment and the dependencies are correctly installed')
-    parser_install.add_argument('-h','--help',action='store_const', const = parser_star.prog.split()[-1],dest='c_help')
+    parser_install.add_argument('-h','--help',action='store_const', const = parser_install.prog.split()[-1],dest='c_help')
     parser_install.set_defaults(func=checkInstall) ;
     
     ### Parsing of command line
