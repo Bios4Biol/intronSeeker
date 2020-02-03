94dd8a884cdb49f59ef0664ef823d2fb59d86631
Sarah Maman-Haddad <sarah.maman-haddad@inra.fr>
2020-02-03 15:31:33 +0100

    Update simulation2HTML.py

--- scripts/simulation2HTML.py
+++ scripts/simulation2HTML.py
@@ -4,17 +4,17 @@ import re
 import pickle
 import os
 import pandas as pd
+import pysam
 import gzip
 import time
 import sys
 import numpy as np
-from datetime import datetime
-from datetime import time as dt_tm
-from datetime import date as dt_date
-#import plotly as py
+import plotly as py
 #import plotly.plotly as py
 #import plotly.tools as plotly_tools
 #import plotly.graph_objs as go
+import configparser
+import concurrent.futures as prl
 import tempfile
 os.environ['MPLCONFIGDIR'] = tempfile.mkdtemp()
 #import matplotlib.pyplot as plt
@@ -22,9 +22,26 @@ os.environ['MPLCONFIGDIR'] = tempfile.mkdtemp()
 from IPython.display import HTML
 from pprint import pprint
 from collections import OrderedDict
-import configparser
-import concurrent.futures as prl
 from itertools import repeat
+from Bio import SeqIO
+from intronSeekerPlot import *
+from highcharts import Highchart # import highchart library
+#ModuleNotFoundError: No module named 'highcharts'
+#(ISeeker_environment) sigenae@genologin2 /work/project/sigenae/sarah/intronSeeker/scripts $ pip install python-highcharts
+#https://github.com/kyper-data/python-highcharts
+
+
+#The plotly.plotly module is deprecated,
+#please install the chart-studio package and use the
+#chart_studio.plotly module instead. 
+
+
+#https://stackoverflow.com/questions/9252543/importerror-cannot-import-name-x
+#  File "/home/sigenae/.conda/envs/ISeeker_environment/lib/python3.6/site-packages/charts/__init__.py", line 5, in <module>
+#    from plot import plot, plotasync, line, area, spline, pie
+#ImportError: cannot import name 'plot'  --> le package charts n ait pas bouge depuis 2015 ... et que les problèmes d'incompatibilité avec Python3 perdurent ...
+#DONC choix d'un package plus actuel.
+#https://github.com/kyper-data/python-highcharts/blob/master/examples/highcharts/Example1.py
 
 
 #step 1  full random simulation : intronSeeker fullRandomSimulation -r -o FRS/ 
@@ -33,49 +50,33 @@ from itertools import repeat
 #module load system/Miniconda3-4.7.10;
 #source activate ISeeker_environment;
 #cd scripts/; 
-#python3 simulation2HTML.py -g ../FRS/frs_modifications.gtf -r ../FRS/frs_contigs-modified.fa -f ../FRS/frs_contigs.fa -o HTML/
+#python3 simulation2HTML.py -g ../FRS/frs_modifications.gtf -r ../FRS/frs_contigs-modified.fa -f ../FRS/frs_contigs.fa -o HTML
 #python3 simulation2HTML.py -g /work/project/sigenae/sarah/intronSeeker/FRS/CAS-A/sample1/frs_sample1_modifications.gtf -r /work/project/sigenae/sarah/intronSeeker/FRS/CAS-A/sample1/frs_sample1_contigs-modified.fa -f /work/project/sigenae/sarah/intronSeeker/FRS/CAS-A/sample1/frs_sample1_contigs.fa -o HTML/
 #simulate reads : config/grinder_frs_testA.cfg
 
 def plot_gtf(gtf_file):
     #Tableau 1 - https://plot.ly/python/table/
-    df = pd.read_fwf(gft_file, sep = "\t")
+    #df = pd.read_fwf(gtf_file, sep = "\t")
+    df = pd.read_table(gtf_file, sep='\t', header = None, names = ['contig', 'frs', 'intron', 'start', 'end', 'x1', 'x2', 'x3', 'x4'])
 
     summary_table_1 = go.Figure(data=[go.Table(
     header=dict(values=list(df.columns),
                 fill_color='paleturquoise',
                 align='left'),
-    cells=dict(values=[df.col1, df.col2, df.col3, df.col4, df.col5, df.col6, df.col7, df.col8, df.col9],
+    cells=dict(values=[df.contig, df.frs, df.intron, df.start, df.end, df.x1, df.x2, df.x3, df.x4],
                fill_color='lavender',
                align='left'))
     ])
-    summary_table_1.show()
+    #summary_table_1.show()
     #Mise en forme du tableau
     summary_table_1 = df.describe()
-    summary_table_1 = summary_table_1.to_html()
+    #summary_table_1 = summary_table_1.to_html()
     return summary_table_1 
-    
-def report_HTML(html_file):
-    f = open(html_file,'w')
-    #Report HTML
-    html_string = '''
-    <html>
-        <head>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-            <style>body{ margin:0 100; background:whitesmoke; }</style>
-        </head>
-        <body>
-            <h1>2014 technology and CPG stock prices</h1>
-            <h3>Summary table: 2014 stock statistics</h3>
-                ''' + summary_table_1 + '''   
-        </body>
-    </html>'''
-    f.write(html_string)
-    f.close()    
-    return f    
-
+   
+   
+#Distribution des tailles d introns
 def parse_length_introns(gtf_file) :
-    table = pd.read_table(gtf_file, sep='\t', header = None, names = ['contig', 'frs', 'intron', 'start', 'end', 'x1', 'x2', 'x3', 'x4'])
+    table = pd.read_table(gtf_file, sep='\t', header = 0, names = ['contig', 'frs', 'intron', 'start', 'end', 'x1', 'x2', 'x3', 'x4'])
     table["length"] = table["end"]-table["start"]
     return table.set_index('length')
 
@@ -87,7 +88,39 @@ def parse_nb_introns_by_contig(gtf_file) :
     for x in l:
         d[x] = d.get(x, 0) + 1
     return d
+    
+def stats_contigs_introns(fa_file, modifiedfa_file, gtf_file):
+    for line in open(fa_file):
+        if line.startswith('>'):
+            nContigs = sum(1 for _ in open(fa_file))
+            nContigsAvecIntrons = sum(1 for _ in open(modifiedfa_file))
+    
+    nIntrons = sum(1 for _ in open(gtf_file))
+     
+    #files and paths
+    figFiles = go.Figure(data=[go.Table(header=dict(values=['data', 'File name/path']),
+                cells=dict(values=[['FRS'], [gtf_file, fa_file, modifiedfa_file]]))
+                    ])
+    #figFiles.show()               
+    #figFiles.to_html()
+    
+    #Stats
+    fig = go.Figure(data=[go.Table(header=dict(values=['Nb contigs/transcripts', 'Nb contigs/transcripts with introns', 'Nb introns']),
+                cells=dict(values=[[nContigsAvecIntrons], [ nContigsAvecIntrons-nContigs], [nIntrons]]))
+                    ])
+    #fig.show()
+    #Mise en forme du tableau
+    #fig = fig.to_html()
+    
+    #Nombre min / max / moyen d'introns retenus par contig
+    nbIntronsByContig=parse_nb_introns_by_contig(gtf_file)
+    print ('Nb introns par contig: ', nbIntronsByContig)
+    #charts.plot(nbIntronsByContig, name='Second list data', show='inline', options=dict(title=dict(text='My second chart!')))
+    #charts.html()
 
+    return nIntrons, nContigsAvecIntrons, nContigs, nbIntronsByContig, fig, figFiles;
+#https://www.geeksforgeeks.org/g-fact-41-multiple-return-values-in-python/
+   
 def simulationReport(fa : str, modifiedfa : str, gtf : str, output : str, prefix : str) :
     output_path = output + "/html";
     if prefix:
@@ -96,36 +129,63 @@ def simulationReport(fa : str, modifiedfa : str, gtf : str, output : str, prefix
     # Create output dir if not exist
     if not os.path.exists(output) :
         os.mkdir(output)
-    
-    for line in open(fa.name):
-        if line.startswith('>'):
-		    nContigs=sum(1 for _ in open(fa.name))
-		    
-	for line in open(fa.name):
-        if line.startswith('>'):    
-            nContigsAvecIntrons = sum(1 for _ in open(modifiedfa.name))
-            
-    print ('Nombre de total de contigs: ' , nContigsAvecIntrons)
-    print ('Nombre de contigs (verif): ' , nContigs)
-    
-    nIntrons = sum(1 for _ in open(gtf.name))
-    print ('Nombre total introns retenus: ' , nIntrons)
-    
-    #Nombre min / max / moyen d'introns retenus par contig
-    nbIntronsByContig=parse_nb_introns_by_contig(gtf.name)
-    print ('Nb introns par contig: ', nbIntronsByContig)
-    
-    #Distribution des tailles d introns
-    lengthDistrib=parse_length_introns(gtf.name)
-    print ('Distribution de la taille des introns: ', lengthDistrib)
-    #print uniquement ligne 1 : print ('Distribution de la taille des introns: ', lengthDistrib.head(1))
 
+    stats_contigs_introns(fa.name, modifiedfa.name, gtf.name)
+       
+    nIntrons, nContigsAvecIntrons, nContigs, nbIntronsByContig, fig, figFiles = stats_contigs_introns(fa.name, modifiedfa.name, gtf.name)
+    
     #create the report in a HTML file
-    #plot_gtf(gtf.name)
-    #report_HTML(output.name)
+    figtable=fig\
+        .to_html()\
+        .replace('<table border="1" class="dataframe">','<table class="table table-striped">') # use bootstrap styl
+    
+    figFilestable=figFiles\
+        .to_html()\
+        .replace('<table border="1" class="dataframe">','<table class="table table-striped">') # use bootstrap styl
+    
+    lengthDistrib=parse_length_introns(gtf.name)
+    lengthDistribTable=lengthDistrib\
+        .to_html()\
+        .replace('<table border="1" class="dataframe">','<table class="table table-striped">') # use bootstrap styl
+            
+    #introns_length_plot_url = py.plot(lengthDistrib, filename='Introns length', auto_open=False,)
+    #filesList_plot_url = py.plot(figFiles, height=1000, width=1000, auto_open=False,\
+    #                      filename='Major technology and CPG stock prices in 2014 - scatter matrix')
+    #<h3>Files list: """ + filesList_plot_url + """</h3>
+	#<h3>Introns lengths: """ + introns_length_plot_url + """</h3>
+
+    reportFile = '%s/%s' % (output,'stats.html')
+    
+    #http://www.xavierdupre.fr/app/ensae_teaching_cs/helpsphinx/notebooks/td1a_cenonce_session_10.html
+    print ('length line 1', lengthDistrib.head(1))
+    #print ('length contig', lengthDistrib.loc["length"])
+    
+    IntronsByContig = Highchart()
+    IntronsByContig.add_data_set(nbIntronsByContig, series_type='line', name='Example Series')
+    IntronsByContig.save_file(reportFile)
+    
+    
+    
+	
+    with open(reportFile,"a+") as f:
+       contenu ="""
+	   <html>
+      <head>
+	   <link rel="stylesheet" href="/work/project/sigenae/sarah/intronSeeker/scripts/bootstrap.min.css">
+	   <style>body{ margin:0 100; background:whitesmoke; }</style>
+	   </head>
+	   <body>
+	   <h1>FRS - STAR / HISAT2 comparison</h1>
+	   <h3>Files list and paths:  """ + figFilestable + """ </h3>
+	   <h3>Introns / Contigs / Transcripts: """ + figtable + """</h3>
+	   <h3>Introns lengths distribution: """ + lengthDistribTable + """</h3>
+       </body>
+       </html>"""
+       f.write(contenu)
+       f.close() 
     
     pass
-
+#plots : https://python-graph-gallery.com/
 
 if __name__ == '__main__' :
     
