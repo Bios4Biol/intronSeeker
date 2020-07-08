44ff9bce4d6e3e0642a7b78bc04aa1b34f4c4958
Sarah Maman <sarah.maman@inra.fr>
2020-07-08 15:24:40 +0200

    Add trimFasta steps in TEST.py

--- data/TEST.py
+++ data/TEST.py
@@ -16,7 +16,7 @@ import configparser # To parse parameters file
 ############
 
 def write_cgf_file(alignment:str, testsDir: str, pipelineName:str, mode:str ):
-	print('Step 8/8 : Simulation Report with '+alignment)
+	print('Report Step : Simulation Report with '+alignment)
 	lowerAlignment = alignment.lower()
 	if (mode == "FRS"):
 		mfasta= testsDir+'/frs_'+pipelineName+'_contigs-modified.fa'
@@ -55,67 +55,76 @@ def mkdirWorkDir(workDir: str, pipelineName:str):
 
 def simulationTests(mode: str, grinder: str, pipelineName: str, workDir:str, gtf:str, fasta:str, settingsFRS: str, settingsGBS:str, settingsSTAR: str, settingsHISAT2: str, settingsSRS: str, settingsTF: str):
 
-	if (mode == "InstallIntronSeeker"):
-		#Installation IntronSeeker
-		os.system('./setup.sh')
+    if (mode == "InstallIntronSeeker"):
+        #Installation IntronSeeker
+        os.system('./setup.sh')
 
-	#Tester l'installation
-	#Vérification de l'installation d'IntronSeeker et des ses dépendances (présence & version) avec le programme intronSeeker checkInstallEdit
-	if (mode == "TestInstallIntronSeeker"):
-		os.system('intronSeeker checkInstall')	
+    #Check installation
+    if (mode == "TestInstallIntronSeeker"):
+        os.system('intronSeeker checkInstall')
 
-	#Full-random data simulation   ==> TODO : re-ecrire https://forgemia.inra.fr/emilien.lasguignes/intronSeeker/blob/master/doc/SIMULATION.md
-	if (mode == "FRS"):
-		print('FRS mode')
-		testsDir = mkdirWorkDir(workDir, pipelineName)
-		print('Step 1/8 : Generate contigs fasta')
-		os.system('intronSeeker fullRandomSimulation '+settingsFRS+' -o '+pipelineName+' -p '+pipelineName)
-		print('Step 2/8 : Generate reads with intronSeeker simulateReads from reference fasta')
-		os.system('intronSeeker simulateReads -o '+pipelineName+' -p '+pipelineName+' -f '+pipelineName+'/frs_'+pipelineName+'_contigs.fa -c '+grinder)
-		print('Step 3/8 : starAlignment')
-		os.system('intronSeeker starAlignment '+settingsSTAR+' -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa -1 '+pipelineName+'/sr_'+pipelineName+'_R1.fastq.gz \
-			    -2 '+pipelineName+'/sr_'+pipelineName+'_R2.fastq.gz -o '+pipelineName+' -p '+pipelineName)
-		print('Step 4/8 : splitReadSearch post STAR')
-		os.system('intronSeeker splitReadSearch '+settingsSRS+' -a '+pipelineName+'/star_'+pipelineName+'.sort.bam \
-			    -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa \
-		        -o '+pipelineName+' -p '+pipelineName+'_STAR')
-		print('Step 5/8 : nOK - trimFastaFromTXT post STAR')
-		print('Step 6/8 : hisat2Alignment')
-		os.system('intronSeeker hisat2Alignment '+settingsHISAT2+' -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa \
-		        -1 '+pipelineName+'/sr_'+pipelineName+'_R1.fastq.gz -2 '+pipelineName+'/sr_'+pipelineName+'_R2.fastq.gz -o '+pipelineName+' -p '+pipelineName)
-		print('Step 7/8 : splitReadSearch post HISAT2')
-		os.system('intronSeeker splitReadSearch '+settingsSRS+' -a '+pipelineName+'/hisat2_'+pipelineName+'.sort.bam \
-		        -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa -o '+pipelineName+' -p '+pipelineName+'_HISAT2')		
-		write_cgf_file('STAR', testsDir, pipelineName, mode)
-		write_cgf_file('HISAT2', testsDir, pipelineName, mode)
-		os.system('python3 /home/smaman/Documents/PROJETS/INTRONSEEKER/intronSeeker/scripts/simulation2HTML.py -F --config_file '+ testsDir+'/'+pipelineName+'_STAR.cfg')
-		os.system('python3 /home/smaman/Documents/PROJETS/INTRONSEEKER/intronSeeker/scripts/simulation2HTML.py -F --config_file '+ testsDir+'/'+pipelineName+'_HISAT2.cfg')
+    #Full-random data simulation   ==> TODO : re-ecrire https://forgemia.inra.fr/emilien.lasguignes/intronSeeker/blob/master/doc/SIMULATION.md
+    if (mode == "FRS"):
+        print('FRS mode')
+        testsDir = mkdirWorkDir(workDir, pipelineName)
+        print('Step 1/8 : Generate contigs fasta')
+        os.system('intronSeeker fullRandomSimulation '+settingsFRS+' -o '+pipelineName+' -p '+pipelineName)
+        print('Step 2/8 : Generate reads with intronSeeker simulateReads from reference fasta')
+        os.system('intronSeeker simulateReads -o '+pipelineName+' -p '+pipelineName+' -f '+pipelineName+'/frs_'+pipelineName+'_contigs.fa -c '+grinder)
+        print('Step 3/8 : starAlignment')
+        os.system('intronSeeker starAlignment '+settingsSTAR+' -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa \
+            -1 '+pipelineName+'/sr_'+pipelineName+'_R1.fastq.gz -2 '+pipelineName+'/sr_'+pipelineName+'_R2.fastq.gz -o '+pipelineName+' -p '+pipelineName)
+        print('Step 4/8 : splitReadSearch post STAR')
+        os.system('intronSeeker splitReadSearch '+settingsSRS+' -a '+pipelineName+'/star_'+pipelineName+'.sort.bam \
+            -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa \
+            -o '+pipelineName+' -p '+pipelineName+'_STAR')
+        print('Step 5/8 : trimFastaFromTXT post STAR')
+        os.system('intronSeeker.py trimFastaFromTXT -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa \
+            -c '+testsDir+'/srs_'+pipelineName+'_STAR_candidates.txt -o '+pipelineName+' -p '+pipelineName)
+        print('Step 6/8 : hisat2Alignment')
+        os.system('intronSeeker hisat2Alignment '+settingsHISAT2+' -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa \
+            -1 '+pipelineName+'/sr_'+pipelineName+'_R1.fastq.gz -2 '+pipelineName+'/sr_'+pipelineName+'_R2.fastq.gz -o '+pipelineName+' -p '+pipelineName)
+        print('Step 7/8 : splitReadSearch post HISAT2')
+        os.system('intronSeeker splitReadSearch '+settingsSRS+' -a '+pipelineName+'/hisat2_'+pipelineName+'.sort.bam \
+            -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa -o '+pipelineName+' -p '+pipelineName+'_HISAT2')	
+        print('Step 8/8 : trimFastaFromTXT post STAR')
+        os.system('intronSeeker.py trimFastaFromTXT -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa \
+            -c '+testsDir+'/srs_'+pipelineName+'_HISAT2_candidates.txt -o '+pipelineName+' -p '+pipelineName)			
+        write_cgf_file('STAR', testsDir, pipelineName, mode)
+        write_cgf_file('HISAT2', testsDir, pipelineName, mode)
+        os.system('python3 /home/smaman/Documents/PROJETS/INTRONSEEKER/intronSeeker/scripts/simulation2HTML.py -F --config_file '+ testsDir+'/'+pipelineName+'_STAR.cfg')
+        os.system('python3 /home/smaman/Documents/PROJETS/INTRONSEEKER/intronSeeker/scripts/simulation2HTML.py -F --config_file '+ testsDir+'/'+pipelineName+'_HISAT2.cfg')
 
 
-	#GBS
-	if (mode == "GBS"):
-		print('GBS mode')
-		testsDir=mkdirWorkDir(workDir, pipelineName)
-		print('Step 1/8 : Generate contigs fasta')
-		os.system('intronSeeker.py GTFbasedSimulation '+settingsGBS+' -a '+gtf+' -r '+fasta+' -p '+pipelineName+' -o '+pipelineName)
-		print('Step 2/8 : Generate reads with intronSeeker simulateReads from reference fasta')
-		os.system('intronSeeker.py simulateReads -f '+pipelineName+'/gbs_'+pipelineName+'_transcripts.fa -c '+grinder+' -p '+pipelineName+' -o '+pipelineName)
-		print('Step 3/8 : starAlignment')
-		os.system('intronSeeker starAlignment -r '+pipelineName+'/gbs_'+pipelineName+'_transcripts-modified.fa -1 '+pipelineName+'/sr_'+pipelineName+'_R1.fastq.gz \
-			       -2 '+pipelineName+'/sr_'+pipelineName+'_R2.fastq.gz -o  -o '+pipelineName+' -t 6   -p '+pipelineName)
-		print('Step 4/8 : splitReadSearch post STAR')
-		os.system('intronSeeker splitReadSearch -a '+pipelineName+'/star_'+pipelineName+'.sort.bam -r '+pipelineName+'/gbs_'+pipelineName+'_transcripts.fa -o starSplit  -o '+pipelineName+' -p '+pipelineName+' -t 6')
-		print('Step 5/8 : nOK - trimFastaFromTXT post STAR')
-		print('Step 6/8 : hisat2Alignment')
-		os.system('intronSeeker hisat2Alignment '+settingsHISAT2+' -r '+pipelineName+'/gbs_'+pipelineName+'_transcripts-modified.fa \
-		        -1 '+pipelineName+'/sr_'+pipelineName+'_R1.fastq.gz -2 '+pipelineName+'/sr_'+pipelineName+'_R2.fastq.gz -o '+pipelineName+' -p '+pipelineName)
-		print('Step 7/8 : splitReadSearch post HISAT2')
-		os.system('intronSeeker splitReadSearch '+settingsSRS+' -a '+pipelineName+'/hisat2_'+pipelineName+'.sort.bam \
-		        -r '+pipelineName+'/gbs_'+pipelineName+'_transcripts-modified.fa -o '+pipelineName+' -p '+pipelineName+'_HISAT2')	
-		write_cgf_file('STAR', testsDir, pipelineName, mode)
-		write_cgf_file('HISAT2', testsDir, pipelineName, mode)
-		os.system('python3 /home/smaman/Documents/PROJETS/INTRONSEEKER/intronSeeker/scripts/simulation2HTML.py -F --config_file '+ testsDir+'/'+pipelineName+'_STAR.cfg')
-		os.system('python3 /home/smaman/Documents/PROJETS/INTRONSEEKER/intronSeeker/scripts/simulation2HTML.py -F --config_file '+ testsDir+'/'+pipelineName+'_HISAT2.cfg')
+    #GBS
+    if (mode == "GBS"):
+        print('GBS mode')
+        testsDir=mkdirWorkDir(workDir, pipelineName)
+        print('Step 1/8 : Generate contigs fasta')
+        os.system('intronSeeker.py GTFbasedSimulation '+settingsGBS+' -a '+gtf+' -r '+fasta+' -p '+pipelineName+' -o '+pipelineName)
+        print('Step 2/8 : Generate reads with intronSeeker simulateReads from reference fasta')
+        os.system('intronSeeker.py simulateReads -f '+pipelineName+'/gbs_'+pipelineName+'_transcripts.fa -c '+grinder+' -p '+pipelineName+' -o '+pipelineName)
+        print('Step 3/8 : starAlignment')
+        os.system('intronSeeker starAlignment -r '+pipelineName+'/gbs_'+pipelineName+'_transcripts-modified.fa -1 '+pipelineName+'/sr_'+pipelineName+'_R1.fastq.gz \
+                -2 '+pipelineName+'/sr_'+pipelineName+'_R2.fastq.gz -o  -o '+pipelineName+' -t 6   -p '+pipelineName)
+        print('Step 4/8 : splitReadSearch post STAR')
+        os.system('intronSeeker splitReadSearch -a '+pipelineName+'/star_'+pipelineName+'.sort.bam -r '+pipelineName+'/gbs_'+pipelineName+'_transcripts.fa -o starSplit  -o '+pipelineName+' -p '+pipelineName+' -t 6')
+        print('Step 5/8 : trimFastaFromTXT post STAR')
+        os.system('intronSeeker.py trimFastaFromTXT -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa \
+                -c '+testsDir+'/srs_'+pipelineName+'_STAR_candidates.txt -o '+pipelineName+' -p '+pipelineName)	
+        print('Step 6/8 : hisat2Alignment')
+        os.system('intronSeeker hisat2Alignment '+settingsHISAT2+' -r '+pipelineName+'/gbs_'+pipelineName+'_transcripts-modified.fa \
+                -1 '+pipelineName+'/sr_'+pipelineName+'_R1.fastq.gz -2 '+pipelineName+'/sr_'+pipelineName+'_R2.fastq.gz -o '+pipelineName+' -p '+pipelineName)
+        print('Step 7/8 : splitReadSearch post HISAT2')
+        os.system('intronSeeker splitReadSearch '+settingsSRS+' -a '+pipelineName+'/hisat2_'+pipelineName+'.sort.bam \
+                -r '+pipelineName+'/gbs_'+pipelineName+'_transcripts-modified.fa -o '+pipelineName+' -p '+pipelineName+'_HISAT2')	
+        print('Step 8/8 : trimFastaFromTXT post HISAT2')
+        os.system('intronSeeker.py trimFastaFromTXT -r '+pipelineName+'/frs_'+pipelineName+'_contigs-modified.fa \
+                -c '+testsDir+'/srs_'+pipelineName+'_HISAT2_candidates.txt -o '+pipelineName+' -p '+pipelineName)			
+        write_cgf_file('STAR', testsDir, pipelineName, mode)
+        write_cgf_file('HISAT2', testsDir, pipelineName, mode)
+        os.system('python3 /home/smaman/Documents/PROJETS/INTRONSEEKER/intronSeeker/scripts/simulation2HTML.py -F --config_file '+ testsDir+'/'+pipelineName+'_STAR.cfg')
+        os.system('python3 /home/smaman/Documents/PROJETS/INTRONSEEKER/intronSeeker/scripts/simulation2HTML.py -F --config_file '+ testsDir+'/'+pipelineName+'_HISAT2.cfg')
 
 
 if __name__ == '__main__' :
