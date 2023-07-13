963456db4bcfef56a9085367dcdbe0ac42bd54cc
Sarah Maman-Haddad <sarah.maman-haddad@inra.fr>
2023-07-13 09:08:17 +0200

    Remove main because checkInstall() is called by intronSeeker command line.

--- scripts/checkInstall.py
+++ scripts/checkInstall.py
@@ -109,7 +109,3 @@ def print_to_stdout(*a):
     # Here a is the array holding the objects
     # passed as the argument of the function
     print(*a, file = sys.stdout)
-
-
-if __name__ == "__main__" :
-    checkInstall() ;
